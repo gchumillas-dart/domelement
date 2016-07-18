eeb6bebd170d1b4f074f0e85b21bde1b48cf2cad
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-18 22:25:30 +0200

    recode

--- lib/src/css_capable.dart
+++ lib/src/css_capable.dart
@@ -1,7 +1,7 @@
 part of domelement;
 
 abstract class CssCapable {
-  Map<String, String> get css => new _CssMap(nativeElement);
+  Map<String, String> get css => new _CssAttributeMap(nativeElement);
 
   set css(Map<String, String> value) {
     css.clear();
@@ -13,59 +13,24 @@ abstract class CssCapable {
   Element get nativeElement;
 }
 
-class _CssMap extends MapBase<String, String> {
+class _CssAttributeMap extends MapBase<String, String> {
   final Element _target;
 
-  _CssMap(this._target);
+  _CssAttributeMap(this._target);
 
-  Iterable<String> get keys => _getCssAttrMap(_target).keys;
-
-  String operator [](Object key) => _getCssAttrMap(_target)[key.toString()];
-
-  void operator []=(String key, String value) {
-    Map<String, String> attrs = _getCssAttrMap(_target);
-    attrs[key] = value;
-    _setCssAttrMap(_target, attrs);
+  Iterable<String> get keys {
+    return new List<String>.generate(
+        _target.style.length, (int i) => _target.style.item(i));
   }
 
-  void clear() => _setCssAttrMap(_target, {});
+  String operator [](Object key) =>
+      _target.style.getPropertyValue(key.toString());
 
-  String remove(Object key) {
-    String ret;
-    Map<String, String> attrs = _getCssAttrMap(_target);
-    ret = attrs.remove(key.toString());
-    _setCssAttrMap(_target, attrs);
-    return ret;
-  }
+  void operator []=(Object key, String value) =>
+      _target.style.setProperty(key.toString(), value);
 
-  Map<String, String> _getCssAttrMap(Element element) {
-    Map<String, String> ret = new Map<String, String>();
-    String css = element.getAttribute('style');
+  void clear() =>
+      keys.forEach((String key) => _target.style.removeProperty(key));
 
-    if (css != null) {
-      css.split(';').forEach((String cssAttr) {
-        List<String> item = cssAttr.split(':');
-        String key = item.length > 0 ? item[0].trim().toLowerCase() : '';
-        String value = item.length > 1 ? item[1].trim() : '';
-
-        if (key.length > 0 && value.length > 0) {
-          ret[key] = value;
-        }
-      });
-    }
-
-    return ret;
-  }
-
-  void _setCssAttrMap(Element element, Map<String, String> attrs) {
-    List<String> arr = [];
-
-    attrs.keys.forEach((String key) {
-      if (key != null && key.length > 0) {
-        arr.add(key.toLowerCase() + ': ' + attrs[key]);
-      }
-    });
-
-    element.setAttribute('style', arr.join('; '));
-  }
+  String remove(Object key) => _target.style.removeProperty(key.toString());
 }
