3986f462937deb901a48e8a22e7f1a80d21d15e4
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-18 19:24:47 +0200

    inline docs

--- lib/core.dart
+++ lib/core.dart
@@ -17,7 +17,12 @@ part 'src/event_capable.dart';
 part 'src/metrics_capable.dart';
 part 'src/null_tree_sanitizer.dart';
 
-DomElement $(dynamic source) {
+/// This function allow to create instances from different sources.
+/// For example:
+///
+///     // creates an instance from a HTML string
+///     $('<span>Some text..</span>');
+DomElement $(dynamic /* String|Element|DomElement */ source) {
   DomElement node;
   if (source is String) {
     node = new DomElement.fromString(source);
@@ -30,10 +35,13 @@ DomElement $(dynamic source) {
   return node;
 }
 
+/// Finds an element from the current document.
+/// This function may return a null value if the element was not found.
 DomElement find(String selectors) {
   Element element = querySelector(selectors);
   return element != null ? new DomElement.fromElement(element) : null;
 }
 
+/// Finds a list of elements from the current document.
 List<DomElement> findAll(String selectors) => querySelectorAll(selectors)
     .map((Element element) => new DomElement.fromElement(element));
