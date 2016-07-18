14ce01dbd410f8082e4254a155f7898fc709a453
soloproyectos <gonzalo.chumillas@gmail.com>
2016-07-18 19:00:17 +0200

    recode

--- test/event/main.dart
+++ test/event/main.dart
@@ -1,13 +1,6 @@
 import 'dart:html';
 import 'package:domelement/core.dart';
 
-/// Processes the 'click' event and deattached it from the target
-void onAnchor3Click(Event event) {
-  DomElement target = new DomElement(event.target);
-  print('Anchor 3: click!');
-  target.off('click', onAnchor3Click);
-}
-
 void main() {
   // Basic example
   find('#anchor1').on('click', () => print('Anchor 1: click!'));
@@ -18,5 +11,12 @@ void main() {
     ..trigger('click', data: 'Hello there!');
 
   // Processes the 'click' event only once
-  find('#anchor3')..on('click', onAnchor3Click);
+  Function listener;
+  listener = (Event event) {
+    DomElement target = new DomElement(event.target);
+    print('Anchor 3: click!');
+    // deattaches the listener from the target
+    target.off('click', listener);
+  };
+  find('#anchor3')..on('click', listener);
 }
